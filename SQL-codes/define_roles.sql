95000ceacf2f3fffcbfd79d5b3e104ac967719a7
Hauke Licht <lichthau@client247-221.wlan.hu-berlin.de>
2016-10-18 17:39:13 +0200

    Update after discussion with team

--- SQL-codes/define_roles.sql
+++ SQL-codes/define_roles.sql
@@ -5,10 +5,18 @@ GRANT usage ON SCHEMA beta_version TO polconfdb_1,polconfdb_2,polconfdb_3,polcon
 GRANT usage ON SCHEMA updates TO polconfdb_1,polconfdb_2,polconfdb_3,polconfdb_4,polconfdb_5 ; 
 
 -- create additional adminstrator role
-GRANT ALL ON SCHEMA public TO polconfdb_1;
-GRANT ALL ON SCHEMA config_data TO polconfdb_1;
-GRANT ALL ON SCHEMA beta_version TO polconfdb_1;
-GRANT ALL ON SCHEMA updates TO polconfdb_1;
+GRANT select, insert, update, delete ON ALL TABLES IN SCHEMA config_data TO polconfdb_1;
+GRANT execute ON ALL FUNCTIONS IN SCHEMA config_data TO polconfdb_1;
+
+GRANT select, insert, update, delete ON ALL TABLES IN SCHEMA beta_version TO polconfdb_1;
+GRANT execute ON ALL FUNCTIONS IN SCHEMA beta_version TO polconfdb_1;
+
+GRANT select, insert, update, delete ON ALL TABLES IN SCHEMA updates TO polconfdb_1;
+GRANT execute ON ALL FUNCTIONS IN SCHEMA updates TO polconfdb_1;
+
+GRANT ALL PRIVILEGES ON SCHEMA config_data TO polconfdb_1;
+GRANT ALL PRIVILEGES ON SCHEMA beta_version TO polconfdb_1;
+GRANT ALL PRIVILEGES ON SCHEMA updates TO polconfdb_1;
 
 -- create two read-and-write accounts
 GRANT select, insert, update, delete ON ALL TABLES IN SCHEMA config_data TO polconfdb_2, polconfdb_3;
